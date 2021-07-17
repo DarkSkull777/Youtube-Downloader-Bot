4f2acd155d4437df8862fec48f7c406855764f57
Dimas Ramadani <59839225+DarkSkull777@users.noreply.github.com>
2021-07-17 07:04:53 +0700

    update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -3,5 +3,5 @@ from pyrogram import Client, Filters
 
 @Client.on_message(Filters.command(["help"]))
 async def start(client, message):
-    helptxt = f"Currently Only supports Youtube Single  (No playlist) Just Send Youtube Url"
+    helptxt = f"Cukup Kirim Url/Link Youtube Ke Chat Ini\nmade by @xskull7"
     await message.reply_text(helptxt)
